edfa11c800a595acec9ebd4474b32b2c8b5e47c5
Fifthaccess <fifthaccess5@gmail.com>
2024-02-28 13:01:54 +0000

    adding sessions

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi import Cookie
 from gitlab import gitlab
- 
+
+import time
+import random
 import json
 import uvicorn
 import requests
@@ -16,9 +18,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+sessions = {}
 
 
 gitlab_dict = {
+    "gitlab_server_url": "https://gitserver.westeurope.cloudapp.azure.com",
     "client_id": "272bc9be7187170fc76cebf13f8c69e7d9f66113632b3f53a664e98c3ac31a1e",
     "client_secret": my_secret["secret"],
     "redirect_uri": "http://appserver-fa.westeurope.cloudapp.azure.com:8080/auth/gitlab",
@@ -26,43 +30,99 @@ gitlab_dict = {
     "state": "STATE",
     "scope": "api",
     "grant_type": "authorization_code",
-    "tokenLifeTime": 900 # 15 in Sekunden
+    "SessionLifeTime": 21600 # 6 Stunden 
 }
 
 class RequiresLoginException(Exception):
     pass
 
+
+
 #____________________________________________________________________________________get Functions_____________________________________________________________________________________________________________________________________________________________________________________
 def getLoginUrl():
-    url = f"https://gitserver.westeurope.cloudapp.azure.com/oauth/authorize?client_id={gitlab_dict['client_id']}&redirect_uri={gitlab_dict['redirect_uri']}&response_type={gitlab_dict['response_type']}&state={gitlab_dict['state']}&scope={gitlab_dict['scope']}"
+    url = f"{gitlab_dict['gitlab_server_url']}/oauth/authorize?client_id={gitlab_dict['client_id']}&redirect_uri={gitlab_dict['redirect_uri']}&response_type={gitlab_dict['response_type']}&state={gitlab_dict['state']}&scope={gitlab_dict['scope']}"
     return url
 
 def getToken(request: Request): 
-    token = request.cookies.get("token")
-    if token:
-        return token 
+    session_id = request.cookies.get("session_id")
+
+    try:
+        token = sessions[int(session_id)]["access_token"]
+        token_creation_time = sessions[int(session_id)]["token_created_at"]
+        token_lifetime = sessions[int(session_id)]["token_lifetime"]
+        session_creation_time = sessions[int(session_id)]["session_created_at"]
+        session_lifetime = gitlab_dict["SessionLifeTime"]
+        
+    except:
+        print("an error has happend")
+        pass
+        #raise RequiresLoginException
+
+    if (time.time() - session_creation_time) > session_lifetime:
+         sessions.pop(int(session_id))
+         raise RequiresLoginException
+    if (time.time() - token_creation_time) < token_lifetime :
+        return token
     else:
-        raise RequiresLoginException
-        #RedirectResponse(url=url)
+        return get_refresh_token(request= request)
+         
+       
+
+
+def get_refresh_token(request: Request):
+    session_id = int(request.cookies.get("session_id"))
+    refresh_token = sessions[session_id]["refresh_token"]
+
+    refresh_url = f"{gitlab_dict['gitlab_server_url']}/oauth/token?client_id={gitlab_dict['client_id']}&client_secret={gitlab_dict['client_secret']}&refresh_token={refresh_token}&grant_type=refresh_token&redirect_uri={gitlab_dict['redirect_uri']}"
+    response2 = requests.post(refresh_url)
+    token = json.loads(response2.content.decode("utf-8"))
+
+    sessions[session_id]["access_token"] = token["access_token"]
+    sessions[session_id]["refresh_token"] = token["refresh_token"]
+    sessions[session_id]["token_created_at"] = token["created_at"]
+    sessions[session_id]["token_lifetime"] = token["expires_in"]
+    print("got an refresh")
+    return token["access_token"]
 
 def getProjects(token):
     gl = gitlab.Gitlab(
-        "https://gitserver.westeurope.cloudapp.azure.com", oauth_token=token
+        gitlab_dict["gitlab_server_url"], oauth_token=token
     )
     gl.auth()
     user = gl.users.list(search='benedikt.erkner-sach')
     print(user)
     projects = gl.projects.list(get_all=True)
     print(projects)
+    project = gl.projects.get(2)
+    storage = project.storage.get()
+    print(storage)
     return projects
 
+
 def getProfile(token):
-    user_info = requests.get("https://gitserver.westeurope.cloudapp.azure.com/api/v4/user", headers={"Authorization": f"Bearer {token}"})
+    user_info = requests.get(f"{gitlab_dict['gitlab_server_url']}/api/v4/user", headers={"Authorization": f"Bearer {token}"})
     return json.loads(user_info.content.decode("utf-8"))
 
     #raise HTTPException(status_code=400, detail="Incorrect Account")
     # gitignore = gl.gitignores.get('Python') für gitignore
     #print(gitignore.content)
+
+
+def create_session(token):
+    session_id = len(sessions) + random.randint(0, 1000000)
+    user = getProfile(token=token["access_token"])
+    session = {
+        "user" : user,
+        "access_token" : token["access_token"],
+        "refresh_token" : token["refresh_token"],
+        "token_created_at" : token["created_at"],
+        "token_lifetime" : token["expires_in"],
+        "session_created_at" : time.time()
+        }
+    
+    sessions[session_id] = session #finished dict
+    return session_id
+
 
 #____________________________________________________________________________________App Exeptions Handler_____________________________________________________________________________________________________________________________________________________________________________________
 
@@ -98,14 +158,15 @@ async def login_gitlab():
 @app.get("/auth/gitlab")
 async def auth_gitlab(request: Request):
     code = request.query_params.get("code")
-    token_url = f"https://gitserver.westeurope.cloudapp.azure.com/oauth/token?client_id={gitlab_dict['client_id']}&client_secret={gitlab_dict['client_secret']}&code={code}&grant_type={gitlab_dict['grant_type']}&redirect_uri={gitlab_dict['redirect_uri']}"
+    token_url = f"{gitlab_dict['gitlab_server_url']}/oauth/token?client_id={gitlab_dict['client_id']}&client_secret={gitlab_dict['client_secret']}&code={code}&grant_type={gitlab_dict['grant_type']}&redirect_uri={gitlab_dict['redirect_uri']}"
     response2 = requests.post(token_url)
-    token = json.loads(response2.content.decode("utf-8"))["access_token"]
+    session_id = create_session(json.loads(response2.content.decode("utf-8")))
+    #token = json.loads(response2.content.decode("utf-8"))["access_token"]
 
-    # tempRes = templates.TemplateResponse(request=request, name="Projekts.j2",context={"token": token})
-    # tempRes.set_cookie(key="token", value=token, expires=120)
+
     RedRes = RedirectResponse(url="/projects")
-    RedRes.set_cookie(key="token", value=token, expires=gitlab_dict["tokenLifeTime"])
+    #RedRes = HTMLResponse("your loged in")
+    RedRes.set_cookie(key="session_id", value=session_id)
    
     return  RedRes  
 
